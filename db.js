75b3c3aac6fa895a8f0366eaee92f89c106200d9
Gemudesakio <79481567+Gemudesakio@users.noreply.github.com>
2023-06-04 00:49:07 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
+// Paquete necesario para conectar a bases de datos MySQL.
+var mysql = require('mysql');
+// Parámetros de conexión a la base de datos
+var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'universidad',
     port: 3306
 });
-
+// Función que permite comprobar la conexión a la base de datos
 connection.connect(function (error) {
 
     if (error) {
@@ -16,11 +18,12 @@ connection.connect(function (error) {
     }
 });
 
+// Función que devolverá resultados de la base de datos
 const consultasDB = ( query ) => {
     return new Promise((resolve, reject) => {
         connection.query(query, (error, results, fields) => {
           if (error) return reject(error);  // <-- Se rechaza la promesa y se pasa el motivo
-          return resolve(results[0]) // <-- Se resuelve la promesa y se para el resultado
+          return resolve(results) // <-- Se resuelve la promesa y se para el resultado
         })
     })
 }
